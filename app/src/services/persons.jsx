45634827de1d680c89155dd1e86173313c925c16
FrJ03 <franjomor@gmail.com>
2024-05-17 13:10:38 +0200

    feat: exercise 11 done: full app to the  internet

--- app/src/services/persons.jsx
+++ app/src/services/persons.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://fullstackopen3-4zuw.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
